ca13d9bbca9c2e36160c048e84c5a3cd10196c82
Bhupesh Dhesingurajan <rajanbhupesh@gmail.com>
2025-04-16 19:04:56 +0530

    new about file creating!!

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
         <Router>
           <Navbar/>
           <Routes>
-            <Route path='/'  element = { <Home /> }/>
+            <Route index path='/'  element = { <Home /> }/>
             <Route path='/menu'  element = { <Menu /> }/>
             <Route path='/about'  element = { <About /> }/>
             <Route path='/contact'  element = { <Contact /> }/>
